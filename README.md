420a9643ed270548b1c24448b98bebe79b19d4c8
sidijju <sidijju@gmail.com>
2025-12-03 23:21:38 +0000

    add dec, wandb, and sweep

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 
 - Run `make setup` and run if its a fresh CPU instance
 - Activate the venv with `source .venv/bin/activate`
+- If you want to activate wandb logging, run 'wandb login'
 - Download data based on each dataset with the relevant make command i.e. `make coco`
 - Change arguments in `config.yaml`
 - Run script with `python3 generate_embeddings.py`
--- generate_embeddings.py
+++ generate_embeddings.py
@@ -13,10 +13,12 @@ import os
 import argparse
 import matplotlib.pyplot as plt
 import tqdm
+import wandb
 
 NUM_WORKERS = os.cpu_count()
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
+DEBUG = False
 
 #### Helpers ####
 
@@ -120,6 +122,35 @@ def save_embeddings(model, dataset, config):
     pq.write_table(table, path)
     print(f"Saved embeddings to {path}")
 
+def save_clusters(config):
+    output_dir = config['output'].get('output_dir','./clip_cpu_model')
+    embedding_path = output_dir + '/embeddings.parquet'
+    centers_path = output_dir + '/dec_centers.pt'
+    clusters_path = output_dir + '/clusters.parquet'
+    
+    df = pd.read_parquet(embedding_path)
+    X = np.stack(df['embedding'].values)
+    
+    centers = torch.load(centers_path)
+    centers = torch.tensor(centers).float()
+
+    X_t = torch.tensor(X)
+
+    dist = ((X_t.unsqueeze(1) - centers.unsqueeze(0)) ** 2).sum(dim=2)
+    q = (1.0 + dist).pow(-1)
+    q = q / q.sum(dim=1, keepdim=True)
+
+    clusters = q.argmax(dim=1).numpy()
+
+    df_out = pd.DataFrame({
+        "label": df["label"],
+        "cluster": clusters,
+        "q": list(q.numpy()),
+    })
+    df_out.to_parquet(out_file)
+
+    print(f"[DEC] Clusters saved to {clusters_path}")
+
 #### Train Loop ####
 
 def plot_losses(losses):
@@ -135,7 +166,56 @@ def plot_losses(losses):
     print(f"Saved training loss plot to {plot_path}")
     plt.close()
 
+def compute_alignment_accuracy(logits, labels): 
+    pred_i2t = logits.argmax(dim=1)
+    pred_t2i = logits.t().argmax(dim=1)
+
+    acc_i2t = (pred_i2t == labels).float().mean()
+    acc_t2i = (pred_t2i == labels).float().mean()
+
+    return 0.5 * (acc_i2t + acc_t2i)
+
+def compute_dec_loss(img_emb, text_emb, centers, epsilon=1e-8):
+    # distances to cluster centers
+    dist_img = ((img_emb.unsqueeze(1) - centers.unsqueeze(0))**2).sum(dim=2)  # (B, K)
+    dist_txt = ((text_emb.unsqueeze(1) - centers.unsqueeze(0))**2).sum(dim=2) # (B, K)
+
+    # student-t kernel soft assignments
+    q_img = (1.0 + dist_img).pow(-1)
+    q_img = q_img / q_img.sum(dim=1, keepdim=True)
+
+    q_text = (1.0 + dist_txt).pow(-1)
+    q_text = q_txt / q_txt.sum(dim=1, keepdim=True)
+
+    # combine image + text q
+    q_combined = torch.cat([q_img, q_txt], dim=0)
+    f_k = q_combined.sum(dim=0)
+
+    # target distribution p
+    p_combined = (q_combined**2) / (f_k + epsilon)
+    p_combined = p_combined / p_combined.sum(dim=1, keepdim=True)
+
+    # split back for image and text
+    B = img_emb.size(0)
+    p_img = p_combined[:B]
+    p_txt = p_combined[B:]
+
+    # KL loss
+    loss_dec_img = (p_img * (torch.log(p_img + epsilon) - torch.log(q_img + epsilon))).sum(dim=1).mean()
+    loss_dec_txt = (p_txt * (torch.log(p_txt + epsilon) - torch.log(q_txt + epsilon))).sum(dim=1).mean()
+    return loss_dec_img + loss_dec_txt
+
 def train(config):
+    # Wandb logging
+    wandb_enabled = config.get("wandb").get("enabled", False)
+    dec_enabled = config['dec'].get('enabled', False)
+    if wandb_enabled:
+        wandb.init(
+            project=config["wandb"].get("project", "clip-unsupervised"),
+            config=config,
+            name="debug" if DEBUG else config["wandb"].get("run_name", "trial")
+        )
+
     dataset = load_dataset(config)
     loader = DataLoader(
         dataset, 
@@ -151,10 +231,18 @@ def train(config):
     processor = CLIPProcessor.from_pretrained(model_name, use_fast=True)
     optimizer = torch.optim.AdamW(model.parameters(), lr=float(config['training'].get('lr', 1e-5)))
 
-    epochs = config['training'].get('epochs', 1)
+    if dec_enabled:
+        # DEC configurations
+        K = config['dec'].get("clusters", 80)
+        lambda_dec = config['dec'].get("lambda", 0.1)
+
+        # learnable cluster centers
+        centers = nn.Parameter(torch.randn(K, 512, device=DEVICE)) # 512 = default CLIP embedding dimension
+        optimizer.add_param_group({"params": centers, "lr": float(config['dec'].get('lr', 1e-4))})
 
     all_losses = []
 
+    epochs = config['training'].get('epochs', 1)
     for epoch in range(epochs):
         for imgs, texts in tqdm.tqdm(loader):
             inputs = processor(
@@ -176,15 +264,44 @@ def train(config):
 
             logits = img_emb @ text_emb.t()
             labels = torch.arange(len(imgs))
-            loss = (nn.CrossEntropyLoss()(logits, labels) + nn.CrossEntropyLoss()(logits.t(), labels)) / 2
+
+            # DEC cluster computations
+            if dec_enabled:
+                loss_dec = compute_dec_loss(img_emb, text_emb, centers)
+
+            loss_clip = (nn.CrossEntropyLoss()(logits, labels) + nn.CrossEntropyLoss()(logits.t(), labels)) / 2
+            if dec_enabled:
+                loss = loss_clip + lambda_dec * loss_dec
+            else:
+                loss = loss_clip
 
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
             all_losses.append(loss.item())
+            clip_alignment_acc = compute_alignment_accuracy(logits, labels)
+            if wandb_enabled:
+                wandb.log({
+                    "loss_total": loss.item(),
+                    "loss_clip": loss_clip.item(),
+                    "clip_alignment_acc": clip_alignment_acc,
+                })
+
+                if dec_enabled:
+                    wandb.log({
+                        "loss_dec": loss_dec.item(),
+                    })
+
+            if DEBUG:
+                break
 
         print(f"Epoch {epoch+1} loss={loss.item():.4f}")
+        if wandb_enabled:
+            wandb.log({
+                "epoch": epoch + 1,
+                "epoch_loss": loss.item()
+            })
 
     out_dir = config['output'].get('output_dir','./clip_cpu_model')
     model.save_pretrained(out_dir)
@@ -192,6 +309,9 @@ def train(config):
     plot_losses(all_losses)
 
     save_embeddings(model, dataset, config)
+    if dec_enabled:
+        torch.save(centers.detach().cpu(), os.path.join(out_dir, "dec_centers.pt"))
+        save_clusters(config)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
